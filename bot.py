97da5da481294bf896e3337980f77e4626f891a1
Nicholas <koalrube@gmail.com>
2024-07-02 22:29:23 -0400

    got more of it working correct

---
+++ bot.py
@@ -0,0 +1,20 @@
+from config import Config
+
+config = Config()
+
+import discord
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+
+bot.run(config.DISCORD_TOKEN)
